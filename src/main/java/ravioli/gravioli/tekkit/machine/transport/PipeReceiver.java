7b11034a00d478fbc922d45163436e715628d8f0
BuildTools <unconfigured@null.spigotmc.org>
2016-12-13 08:35:51 -0600

    Clean

--- src/main/java/ravioli/gravioli/tekkit/machine/transport/PipeReceiver.java
+++
@@ -1,8 +0,0 @@
-package ravioli.gravioli.tekkit.machine.transport;
-
-import org.bukkit.block.BlockFace;
-
-public interface PipeReceiver {
-    public boolean canReceiveItem(MovingItem item, BlockFace input);
-    public void addItem(MovingItem item, BlockFace input);
-}
